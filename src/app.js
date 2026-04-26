1867c11a9703d51cdcd38c9ad66abb11f299e264
Santiago <santiagochimko@gmail.com>
2026-04-26 19:52:33 -0300

    fix: update CORS origin URL for backend deployment

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 const corsOptions = {
     origin: [
         "http://localhost:3000",
-        "https://expense-manager-backend-olenrq8ff-santiagochimkos-projects.vercel.app",
+        "https://expense-manager-backend-delta.vercel.app/",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
